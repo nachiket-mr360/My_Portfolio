a4dfb5d2e55312e8b2d2dc38e2b8715d26ae3945
nachiket <nachiket360chavan@gmail.com>
2025-10-31 21:57:36 +0530

    Updates

--- assets/html2canvas-CK_Y7O9x.js
+++ assets/html2canvas-CK_Y7O9x.js
@@ -1,4 +1,4 @@
-import{n as e}from"./index-DbdvvbCR.js";var t=e(((e,t)=>{
+import{n as e}from"./index-CHUHDP2I.js";var t=e(((e,t)=>{
 /*!
 * html2canvas 1.4.1 <https://html2canvas.hertzen.com>
 * Copyright (c) 2022 Niklas von Hertzen <https://hertzen.com>
